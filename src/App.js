c4bc6708ee1a7af98e550808a3e7a4ced521fbed
unknown <ebinan10@gmail.com>
2022-06-12 14:27:02 +0100

    adding first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Homepage } from "./pages/Homepage";
+
+function App() {
+  return (
+    <div className="App">
+     <Homepage></Homepage>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+ import React from 'react'
+import Navbar from '../components/Navbar'
+import Slide from '../components/Slide'
+ 
+ 
+ export const Homepage = () => {
+     return (
+         <div>
+            <Navbar/>
+            <Slide/>
+         </div>
+     )
+ }
+ 
